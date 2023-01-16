cbf9e25e55e42de933251d5d966f3e50873e43a4
Raphael Oliveira <raphael.foliveira1@gmail.com>
2023-01-16 20:41:11 -0300

    Add default port

--- app.ts
+++ app.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import router from "./routes/routes";
 
 dotenv.config();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8000;
 
 const app = express();
 app.use(json());
--- controllers/scraper.controller.ts
+++ controllers/scraper.controller.ts
@@ -7,6 +7,12 @@ export default class ScraperController {
 		const scraper = await setupScraper();
 		
 		const products = await scraper.run(brand);
+
+		if (products.length === 0) {
+			return res.status(504).json({
+				error: "Request timed out."
+			});
+		}
 		
 		return res.status(200).json({
 			products
